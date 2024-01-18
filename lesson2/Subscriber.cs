8e3839c35655616dade8544a11e378c047cb2273
NightOOwl <titov.arsenij@yandex.ru>
2024-01-18 15:20:49 +0300

    Добавьте файлы проекта.

---
+++ lesson2/Subscriber.cs
@@ -0,0 +1,14 @@
+﻿using lesson2.Models;
+
+namespace lesson2
+{
+    public static class Subscriber
+    {
+        private static List<string> _updates = new List<string>(); 
+        public static void  CatalogUpdatesCheck(Product product)
+        {
+            _updates.Add ($"{product.ToString()} {DateTime.Now}");
+        }
+        
+    }
+}
